84c6a37bce74cb1b57d9c44d84ee2d71e5e016fc
Alexey Stepanov <Arsennikum@gmail.com>
2023-11-04 19:38:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from telebot.types import BotCommand, WebAppInfo, InlineKeyboardMarkup, InlineKe
 
 TOKEN = '****' # create token through https://t.me/BotFather
 
-bot = telebot.TeleBot(  TOKEN)
+bot = telebot.TeleBot(TOKEN)
 
 bot.set_my_commands(
     commands=[
         BotCommand(command='/help', description='FAQ'),
         BotCommand(command='/start', description='Старт'),
-        BotCommand(command='/webform', description='Веб форма')
+        BotCommand(command='/webform', description='Написать обращение')
     ]
 )
 
@@ -21,16 +21,18 @@ def comand_hendler(message):
 
 Здесь вы можете задать любой вопрос, касающийся нашей деятельности и конкурса.
 
-💬 Напишите свой вопрос, и мы постараемся ответить на него максимально быстро. Для этого вам необходимо нажать в левом нижнем углу «Меню» — «Webform», и ввести ваш вопрос.''')
+💬 Напишите свой вопрос, и мы постараемся ответить на него максимально быстро. Для этого вам необходимо нажать в левом нижнем углу «Меню» — «Написать обращение», и ввести ваш вопрос.''')
 
-@bot.message_handler(commands=['/help', '/Help', '/FAQ', '/faq'])
+@bot.message_handler(commands=['help', 'Help', 'FAQ', 'faq'])
 def help_handler(message):
-    bot.send_message(message.chat.id, 'Тут будет FAQ по боту')
+    bot.send_message(message.chat.id, '''
+💬 Нажмите в левом нижнем углу кнопку «Меню», затем «Написать обращение», и введите ваш вопрос в открывшуюся форму обращения.
+Пишите свой вопрос только через эту форму. Сообщения, отправленные просто в бот, никто не увидит. Пользуйтесь командами бота.''')
 
 @bot.message_handler(commands=['webForm', 'webform'])
 def start_web_form(message):
     markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton('Веб форма', web_app=WebAppInfo(url='https://forms.yandex.ru/u/653a3e6ee010db2c10cf9912/')))
+    markup.add(InlineKeyboardButton('Форма обращения', web_app=WebAppInfo(url='https://forms.yandex.ru/u/653a3e6ee010db2c10cf9912/')))
     bot.send_message(message.chat.id, 'Нажми на кнопку снизу сообщения', reply_markup=markup)
 
 bot.infinity_polling()
